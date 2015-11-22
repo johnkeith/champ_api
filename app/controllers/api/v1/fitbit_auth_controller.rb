88a9940bd5436718c9c366c20a4dda5a84e1126c
John Keith <johnkeith@outlook.com>
2015-11-21 22:24:35 -0500

    add route for manually doing final round of auth flow

--- app/controllers/api/v1/fitbit_auth_controller.rb
+++ app/controllers/api/v1/fitbit_auth_controller.rb
@@ -6,27 +6,31 @@ module Api
 					CONFIG[Sinatra::Base.environment][:fitbit]
 				end
 
-				def external_fitbit_auth_url					
+				def external_fitbit_auth_url(state)				
 					url = fitbit_config[:oauth_authorization_uri]
 					uri = URI.parse(url)
 					
+					redirect_uri = state ? state : fitbit_config[:oauth_redirect_uri]
+
 					uri.query = URI.encode_www_form(
 						client_id: fitbit_config[:client_id],
 						response_type: 'code',
 						scope: 'activity heartrate profile',
-						redirect_uri: fitbit_config[:oauth_redirect_uri])
+						redirect_uri: redirect_uri)
 					
 					uri.to_s
 				end
 
-				def external_fitbit_access_token_request(code)
+				def external_fitbit_access_token_request(code, state)
 					url = fitbit_config[:oauth_refresh_token_uri]
 					uri = URI.parse(url)
 					
+					redirect_uri = state ? state : fitbit_config[:oauth_redirect_uri]
+
 					uri.query = URI.encode_www_form(
 						client_id: fitbit_config[:client_id],
 						grant_type: 'authorization_code',
-						redirect_uri: fitbit_config[:oauth_redirect_uri],
+						redirect_uri: redirect_uri,
 						code: code)
 
 					req = Net::HTTP::Post.new(uri.request_uri)
@@ -35,15 +39,20 @@ module Api
 					res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |r| r.request(req) }
 					res.body
 				end
+
 			end
 
 			# http://localhost:9393/api/v1/fitbit/auth
 			get '/' do
-				redirect external_fitbit_auth_url
+				redirect external_fitbit_auth_url(params['state'])
 			end
 
 			get '/redirect' do
-				json data: JSON.parse(external_fitbit_access_token_request(params['code']))
+				json data: JSON.parse(external_fitbit_access_token_request(params['code'], params['state']))
+			end
+
+			post '/' do
+				json data: JSON.parse(external_fitbit_access_token_request(params['code'], params['state']))
 			end
 		end
 	end
